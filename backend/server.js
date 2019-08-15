a7837fda776cb7e261553fc6429db58943f1bd99
Franklin Liu <franklin.liu@autodata.net>
2019-08-15 14:06:03 -0400

    created route files

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
 
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server stared on port ${PORT}`));
