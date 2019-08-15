e9250f6619c5acb0cdf6f0a592907a63eb5b0e2c
Franklin Liu <franklin.liu@autodata.net>
2019-08-15 15:23:40 -0400

    login route added

--- backend/routes/api/auth.js
+++ backend/routes/api/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
-
+const config = require('config');
+const { check, validationResult } = require("express-validator");
 
 //@route    GET api/auth
 //@desc     Test route 
@@ -18,5 +20,72 @@ router.get('/', auth, async (req, res) => {
     }
 });
 
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+router.post(
+    "/",
+    [
+        check("email", "Please include a valid email").isEmail(),
+        check(
+            "password",
+            "Password is required"
+        ).exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+
+        const {
+            email,
+            password
+        } = req.body;
+
+        try {
+            let user = await User.findOne({
+                email
+            });
+            if (!user) {
+                res.status(400).json({
+                    errors: [{
+                        msg: "invalid credentials"
+                    }]
+                });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                res.status(400).json({
+                    errors: [{
+                        msg: "invalid credentials"
+                    }]
+                });
+            }
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(payload, config.get("jwtToken"), {
+                expiresIn: 3600
+            }, (err, token) => {
+                if (err) throw err;
+                res.json({
+                    token
+                });
+            });
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server error");
+        }
+    }
+);
 
 module.exports = router; 
