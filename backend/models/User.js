e26a3d0a6b5f812628f97e4db45aa92b8b9eeeb4
Franklin Liu <franklin.liu@autodata.net>
2019-08-16 10:28:09 -0400

    added put request for all arrays

--- backend/models/User.js
+++ backend/models/User.js
@@ -17,7 +17,109 @@ const UserSchema = new mongoose.Schema({
     date: {
         type: Date,
         default: Date.now
-    }
+    },
+    toApply: [
+        {
+            companyName: {
+                type: String,
+                required: true
+            },
+            jobTitle : {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String
+            },
+            applicationUrl: {
+                type: String
+            },
+            deadlineDate :{
+                type: Date
+            }
+        }
+    ],
+    appliedTo: [
+        {
+            companyName: {
+                type: String,
+                required: true
+            },
+            jobTitle : {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String
+            },
+            applicationUrl: {
+                type: String
+            },
+            appliedDate: {
+                type: Date
+            }
+        }
+    ],
+    interview: [
+        {
+            companyName: {
+                type: String,
+                required: true
+            },
+            jobTitle : {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String
+            },
+            applicationUrl: {
+                type: String
+            },
+            interviewDate: {
+                type: Date
+            }
+        }
+    ],
+    offered: [
+        {
+            companyName: {
+                type: String,
+                required: true
+            },
+            jobTitle : {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String
+            },
+            applicationUrl: {
+                type: String
+            },
+            offerDeadlineDate: {
+                type: Date
+            }
+        }
+    ],
+    rejected: [
+        {
+            companyName: {
+                type: String,
+                required: true
+            },
+            jobTitle : {
+                type: String,
+                required: true
+            },
+            location: {
+                type: String
+            },
+            applicationUrl: {
+                type: String
+            }
+        }
+    ]
 });
 
 module.exports = User = mongoose.model('user', UserSchema);
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require("express-validator");
-
+const auth = require('../../middleware/auth');
 const User = require("../../models/User");
 
 // @route   POST api/users
@@ -67,5 +67,207 @@ router.post(
     }
   }
 );
+
+// @route   POST api/users/toApply
+// @desc    populate toApply array
+// @access  Private
+router.put('/toApply', [auth, [
+  check('companyName', 'company name is required').not().isEmpty(),
+  check('jobTitle', 'job title is required').not().isEmpty(),
+]],  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array() });
+    }
+
+    const {
+      companyName,
+      jobTitle, 
+      location,
+      applicationUrl,
+      deadlineDate
+    } = req.body;
+
+    const newToApply = {
+      companyName,
+      jobTitle, 
+      location,
+      applicationUrl,
+      deadlineDate
+    }
+
+    try{
+      const user = await User.findById(req.user.id).select('-password');
+      user.toApply.unshift(newToApply);
+      
+      await user.save();
+      res.json(user);
+    }catch(err){
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+})
+
+
+// @route   POST api/users/appliedTo
+// @desc    populate appliedTo array
+// @access  Private
+router.put('/appliedTo', [auth, [
+  check('companyName', 'company name is required').not().isEmpty(),
+  check('jobTitle', 'job title is required').not().isEmpty(),
+]],  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array() });
+    }
+
+    const {
+      companyName,
+      jobTitle, 
+      location,
+      applicationUrl,
+      appliedDate
+    } = req.body;
+
+    const newAppliedTo = {
+      companyName,
+      jobTitle, 
+      location,
+      applicationUrl,
+      appliedDate
+    }
+
+    try{
+      const user = await User.findById(req.user.id).select('-password');
+      user.appliedTo.unshift(newAppliedTo);
+      
+      await user.save();
+      res.json(user);
+    }catch(err){
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+})
+
+
+// @route   POST api/users/interview
+// @desc    populate interview array
+// @access  Private
+router.put('/interview', [auth, [
+  check('companyName', 'company name is required').not().isEmpty(),
+  check('jobTitle', 'job title is required').not().isEmpty(),
+]],  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array() });
+    }
+
+    const {
+      companyName,
+      jobTitle, 
+      location,
+      applicationUrl,
+      interviewDate
+    } = req.body;
+
+    const newInterview = {
+      companyName,
+      jobTitle, 
+      location,
+      applicationUrl,
+      interviewDate
+    }
+
+    try{
+      const user = await User.findById(req.user.id).select('-password');
+      user.interview.unshift(newInterview);
+      
+      await user.save();
+      res.json(user);
+    }catch(err){
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+})
+
+
+// @route   POST api/users/offered
+// @desc    populate offered array
+// @access  Private
+router.put('/offered', [auth, [
+  check('companyName', 'company name is required').not().isEmpty(),
+  check('jobTitle', 'job title is required').not().isEmpty(),
+]],  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array() });
+    }
+
+    const {
+      companyName,
+      jobTitle, 
+      location,
+      applicationUrl,
+      offerDeadlineDate
+    } = req.body;
+
+    const newOffered = {
+      companyName,
+      jobTitle, 
+      location,
+      applicationUrl,
+      offerDeadlineDate
+    }
+
+    try{
+      const user = await User.findById(req.user.id).select('-password');
+      user.offered.unshift(newOffered);
+      
+      await user.save();
+      res.json(user);
+    }catch(err){
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+})
+
+
+// @route   POST api/users/rejected
+// @desc    populate rejected array
+// @access  Private
+router.put('/rejected', [auth, [
+  check('companyName', 'company name is required').not().isEmpty(),
+  check('jobTitle', 'job title is required').not().isEmpty(),
+]],  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array() });
+    }
+
+    const {
+      companyName,
+      jobTitle, 
+      location,
+      applicationUrl
+    } = req.body;
+
+    const newRejected = {
+      companyName,
+      jobTitle, 
+      location,
+      applicationUrl
+    }
+
+    try{
+      const user = await User.findById(req.user.id).select('-password');
+      user.rejected.unshift(newRejected);
+      
+      await user.save();
+      res.json(user);
+    }catch(err){
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+})
 
 module.exports = router;
