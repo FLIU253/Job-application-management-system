2d015c03965a66a342971f7f2b54518ab4cd76d7
Franklin Liu <franklin.liu@autodata.net>
2019-08-16 16:10:08 -0400

    partially designed login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
 import HomePage from './pages/homepage.component';
-
+import Login from './pages/login.component';
 
 export default class App extends Component{
   render() {
     return(
-      <HomePage/>
+      <Login/>
     );
   }
 }
--- frontend/src/pages/login.component.jsx
+++ frontend/src/pages/login.component.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Login = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 80vh;
+`;
+const InputSection = styled.input`
+  border-radius: 3px;
+  box-shadow: none;
+  border: 0px none;
+  width: 325px;
+  padding: 1em 1em;
+  font-size: 1.1em;
+`;
+
+const SignInButton = styled.button`
+  transition: all 0.15s ease-out 0s;
+  background: transparent;
+  color: #fff;
+  border: 1px solid #fff;
+  width: 250px;
+  padding: 1em;
+  font-size: 1.1em;
+  border-radius: 3px;
+  cursor: pointer;
+  margin-top: 15px;
+`;
 const LoginPage = () => (
-  <div>
-      Testingasd asdasdasdasdas
-  </div>
+  <Login>
+      <div style = {{margin: '0.5em 0'}}>
+      <InputSection type="email" required/>
+      </div>
+      <div style = {{margin: '0.5em 0'}}> 
+      <InputSection type="password" required/>
+      </div>
+      <SignInButton>Login</SignInButton>
+      <a href="/">Create new account</a>
+  </Login>
 );
 
 export default LoginPage;
