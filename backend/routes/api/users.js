e2f76e897c21a8f3fb8ba0e111f900b3e20bf3a4
Franklin Liu <franklin.liu@autodata.net>
2019-08-23 13:51:22 -0400

    Edit routes added for backend

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -445,3 +445,207 @@ router.get('/rejected', auth, async (req, res) => {
   }
 })
 module.exports = router;
+
+
+//@route  update api/users/toApply/:id
+//@desc   update an toApply array
+//@access Private
+router.put('/toApply/:id', [auth, [
+  check('companyName', 'company name is required').not().isEmpty(),
+  check('jobTitle', 'job title is required').not().isEmpty(),
+]],  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array() });
+    }
+
+    const {
+      companyName,
+      jobTitle, 
+      location,
+      applicationUrl,
+      deadlineDate
+    } = req.body;
+
+    try{
+      const user = await User.findById(req.user.id).select('toApply');
+      const matched = user.toApply.find((item) => {
+        return item.id === req.params.id
+      })
+      const index = user.toApply.indexOf(matched);
+      console.log(index);
+      user.toApply[index].companyName = companyName;
+      user.toApply[index].jobTitle = jobTitle;
+      user.toApply[index].location = location;
+      user.toApply[index].applicationUrl = applicationUrl;
+      user.toApply[index].deadlineDate = deadlineDate;
+
+      await user.save();
+      res.json(user);
+    }catch(err){
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+})
+
+//@route  update api/users/appliedTo/:id
+//@desc   update an appliedTo array
+//@access Private
+router.put('/appliedTo/:id', [auth, [
+  check('companyName', 'company name is required').not().isEmpty(),
+  check('jobTitle', 'job title is required').not().isEmpty(),
+]],  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array() });
+    }
+
+    const {
+      companyName,
+      jobTitle, 
+      location,
+      applicationUrl,
+      appliedDate
+    } = req.body;
+
+    try{
+      const user = await User.findById(req.user.id).select('appliedTo');
+      const matched = user.appliedTo.find((item) => {
+        return item.id === req.params.id
+      })
+      const index = user.appliedTo.indexOf(matched);
+      console.log(index);
+      user.appliedTo[index].companyName = companyName;
+      user.appliedTo[index].jobTitle = jobTitle;
+      user.appliedTo[index].location = location;
+      user.appliedTo[index].applicationUrl = applicationUrl;
+      user.appliedTo[index].appliedDate = appliedDate;
+
+      await user.save();
+      res.json(user);
+    }catch(err){
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+})
+
+//@route  update api/users/interview/:id
+//@desc   update an interview array
+//@access Private
+router.put('/interview/:id', [auth, [
+  check('companyName', 'company name is required').not().isEmpty(),
+  check('jobTitle', 'job title is required').not().isEmpty(),
+]],  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array() });
+    }
+
+    const {
+      companyName,
+      jobTitle, 
+      location,
+      applicationUrl,
+      interviewDate
+    } = req.body;
+
+    try{
+      const user = await User.findById(req.user.id).select('interview');
+      const matched = user.interview.find((item) => {
+        return item.id === req.params.id
+      })
+      const index = user.interview.indexOf(matched);
+      console.log(index);
+      user.interview[index].companyName = companyName;
+      user.interview[index].jobTitle = jobTitle;
+      user.interview[index].location = location;
+      user.interview[index].applicationUrl = applicationUrl;
+      user.interview[index].interviewDate = interviewDate;
+
+      await user.save();
+      res.json(user);
+    }catch(err){
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+})
+
+//@route  update api/users/offered/:id
+//@desc   update an offered array
+//@access Private
+router.put('/offered/:id', [auth, [
+  check('companyName', 'company name is required').not().isEmpty(),
+  check('jobTitle', 'job title is required').not().isEmpty(),
+]],  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array() });
+    }
+
+    const {
+      companyName,
+      jobTitle, 
+      location,
+      applicationUrl,
+      offerDeadlineDate
+    } = req.body;
+
+    try{
+      const user = await User.findById(req.user.id).select('offered');
+      const matched = user.offered.find((item) => {
+        return item.id === req.params.id
+      })
+      const index = user.offered.indexOf(matched);
+      console.log(index);
+      user.offered[index].companyName = companyName;
+      user.offered[index].jobTitle = jobTitle;
+      user.offered[index].location = location;
+      user.offered[index].applicationUrl = applicationUrl;
+      user.offered[index].offerDeadlineDate = offerDeadlineDate;
+
+      await user.save();
+      res.json(user);
+    }catch(err){
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+})
+
+//@route  update api/users/rejected/:id
+//@desc   update an rejected array
+//@access Private
+router.put('/rejected/:id', [auth, [
+  check('companyName', 'company name is required').not().isEmpty(),
+  check('jobTitle', 'job title is required').not().isEmpty(),
+]],  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array() });
+    }
+
+    const {
+      companyName,
+      jobTitle, 
+      location,
+      applicationUrl
+    } = req.body;
+
+    try{
+      const user = await User.findById(req.user.id).select('rejected');
+      const matched = user.rejected.find((item) => {
+        return item.id === req.params.id
+      })
+      const index = user.rejected.indexOf(matched);
+      console.log(index);
+      user.rejected[index].companyName = companyName;
+      user.rejected[index].jobTitle = jobTitle;
+      user.rejected[index].location = location;
+      user.rejected[index].applicationUrl = applicationUrl;
+
+      await user.save();
+      res.json(user);
+    }catch(err){
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+})
