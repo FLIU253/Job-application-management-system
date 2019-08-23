61ed819f6da7038a7db649ca7db8932652654bbc
Franklin Liu <franklin.liu@autodata.net>
2019-08-23 11:06:05 -0400

    styled buttons and forms

--- frontend/src/components/add-form.component.jsx
+++ frontend/src/components/add-form.component.jsx
@@ -2,22 +2,55 @@ import React from 'react';
 import styled from 'styled-components';
 
 
-const Cancel = styled.span`
+const Cancel = styled.button`
     cursor: pointer;
+    background-image: linear-gradient(to right, #eb3941, #f15e64, #e14e53, #e2373f);  
+    box-shadow: 0 5px 15px rgba(242, 97, 103, .4);
+    font-weight: 600;
+    color: #fff;
+    cursor: pointer;
+    border: none;
+    border-radius: 50px;
+    width: 75px;
+    height: 25px;
+`;
+const Input = styled.input`
+  border-radius: 60px;
+  box-shadow: none;
+  border: 0px none;
+  padding: 0.5em 0.5em;
+
+`;
+const InputText = styled.p`
+  font: bold;
+  color: #0546ad;
+`;
+
+const Add = styled.button`
+  text-align: center;
+  background-image: linear-gradient(to right, #29323c, #485563, #2b5876, #4e4376);
+  box-shadow: 0 4px 15px 0 rgba(45, 54, 65, 0.75);
+  font-weight: 600;
+  color: #fff;
+  cursor: pointer;
+  border: none;
+  border-radius: 50px;
+  width: 75px;
+  height: 25px;
 `;
 
 const AddForm = ({submitForm, cancelForm, handleChange, companyName, jobTitle, applicationUrl, location, date, rejected, dateText}) => {
   return(
     <form onSubmit = {submitForm}>
-    <p>Company Name:</p> <input type="text"  name =  "companyName"  value = {companyName} onChange = {handleChange} required/>
-    <p>Position Name:</p> <input type="text" name =  "jobTitle" value = {jobTitle} onChange = {handleChange} required/>
-    <p>Application Link:</p> <input type="text" name =  "applicationUrl" value = {applicationUrl} onChange = {handleChange} />
-    <p>Location:</p> <input type="text"  name =  "location" value = {location} onChange = {handleChange}/>
+    <InputText>Company Name:</InputText> <Input type="text"  name =  "companyName"  value = {companyName} onChange = {handleChange} required/>
+    <InputText>Position Name:</InputText> <Input type="text" name =  "jobTitle" value = {jobTitle} onChange = {handleChange} required/>
+    <InputText>Application Link:</InputText> <Input type="text" name =  "applicationUrl" value = {applicationUrl} onChange = {handleChange} />
+    <InputText>Location:</InputText> <Input type="text"  name =  "location" value = {location} onChange = {handleChange}/>
     {
-        !rejected ? (<div><p>{dateText}</p> <input type="date" name =  "date" value = {date} onChange = {handleChange} /></div>) : null
+        !rejected ? (<div><InputText>{dateText}</InputText> <Input type="date" name =  "date" value = {date} onChange = {handleChange} /></div>) : null
     }
     <br/>
-    <button>Add</button>
+    <Add>Add</Add>
     <span> or </span>
     <Cancel onClick = { cancelForm}> Cancel </Cancel>
    </form>
