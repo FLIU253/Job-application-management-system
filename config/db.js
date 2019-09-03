8537cbb2f10bbdad3b9cd555c6cca39b61680ecd
Franklin Liu <franklin.liu@autodata.net>
2019-09-03 15:28:22 -0400

    minor change

--- config/db.js
+++ config/db.js
@@ -3,17 +3,19 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try{
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useCreateIndex: true
-        });
-        console.log('MongoDB connected..');
-    }catch (err){
-        console.log(err.message);
-        //Exit process with failure
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
