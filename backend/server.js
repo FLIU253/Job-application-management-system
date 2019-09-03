4bbe5f63ff052ba80bf5a2839a76006106b9ba75
Franklin Liu <franklin.liu@autodata.net>
2019-09-03 14:18:55 -0400

    getting ready for build

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+      console.log('User Disconnected');
+    });
+    socket.on('example_message', function(msg){
+      console.log('message: ' + msg);
+    });
+  });
+io.listen(8000);
 
 //Connect database
 connectDB();
@@ -9,12 +23,19 @@ connectDB();
 //Init Middleware
 app.use(express.json({extended: false}));
 
-app.get('/', (req, res) => res.send('API running'));
-
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api', require('./routes/api/resume'));
+
+//Serve static assets in production
+if(process.env.NODE_ENV === 'production' ){
+  //Set static folder
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
