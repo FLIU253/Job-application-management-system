691ae9083018e4b8ba5a24962f3ad860b3e42814
Franklin Liu <franklin.liu@autodata.net>
2019-09-03 15:03:41 -0400

    got rid of useless code

--- server.js
+++ server.js
@@ -4,19 +4,6 @@ const path = require('path');
 
 const app = express();
 
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-io.on('connection', function(socket){
-    console.log('a user connected');
-    socket.on('disconnect', function(){
-      console.log('User Disconnected');
-    });
-    socket.on('example_message', function(msg){
-      console.log('message: ' + msg);
-    });
-  });
-io.listen(8000);
-
 //Connect database
 connectDB();
 
@@ -24,14 +11,15 @@ connectDB();
 app.use(express.json({extended: false}));
 
 //Define Routes
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api', require('./routes/api/resume'))
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api', require('./routes/api/resume'));
 
-//Serve static assets in production
-if(process.env.NODE_ENV === 'production' ){
-  //Set static folder
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
   app.use(express.static('client/build'));
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
