b93f4bcc808195fc5a53fd36fac77b3248106510
Franklin Liu <franklin.liu@autodata.net>
2019-09-04 12:56:25 -0400

    minor fix

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = process.env.MONGO_URI || config.get('mongoURI');
+const db = process.env.MONGOLAB_URI || config.get('mongoURI');
 
 const connectDB = async () => {
   try {
